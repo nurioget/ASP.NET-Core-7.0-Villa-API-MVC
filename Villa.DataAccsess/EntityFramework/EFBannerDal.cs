7c11c6302844bde4706775c30170a2e941c064f3
nurioget <nurioget9@gmail.com>
2024-11-19 19:12:11 +0300

    DataAccess Layer Completion

---
+++ Villa.DataAccsess/EntityFramework/EFBannerDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Villa.DataAccsess.Abstract;
+using Villa.DataAccsess.Context;
+using Villa.DataAccsess.Repositories;
+using Villa.Entity.Entities;
+
+namespace Villa.DataAccsess.EntityFramework
+{
+    public class EFBannerDal : GenericRepository<Banner>, IBannerDal
+    {
+        public EFBannerDal(VillaContext context) : base(context)
+        {
+        }
+    }
+}
