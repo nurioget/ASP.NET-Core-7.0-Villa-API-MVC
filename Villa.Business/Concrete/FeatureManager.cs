3a009cc914ddebc8a90d42c815f5f806227f60ec
nurioget <nurioget9@gmail.com>
2024-11-21 13:07:43 +0300

    Creating Business Layer and Interfaces

---
+++ Villa.Business/Concrete/FeatureManager.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Villa.Business.Abstract;
+using Villa.DataAccsess.Abstract;
+using Villa.Entity.Entities;
+
+namespace Villa.Business.Concrete
+{
+    public class FeatureManager : GenericManager<Feature>, IFeatureService
+    {
+        public FeatureManager(IGenericDal<Feature> genericDal) : base(genericDal)
+        {
+        }
+    }
+}
