95637858f2626773cdc27e082f4c823000b9b47d
nurioget <nurioget9@gmail.com>
2024-11-22 15:59:24 +0300

    CreateBanner and Insert

--- Villa.WebUI/Controllers/BannerController.cs
+++ Villa.WebUI/Controllers/BannerController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using Villa.Business.Abstract;
 using Villa.Dto.Dtos.BannerDtos;
+using Villa.Entity.Entities;
 
 namespace Villa.WebUI.Controllers
 {
@@ -19,8 +21,28 @@ namespace Villa.WebUI.Controllers
         public async Task<IActionResult> Index()
         {
             var values = await _bannerService.TGetListAsync();
-            var bannerList=_mapper.Map<List<ResultBannerDto>>(values);  
+            var bannerList = _mapper.Map<List<ResultBannerDto>>(values);
             return View(bannerList);
+        }
+
+        public async Task<IActionResult> DeleteBanner(ObjectId id)
+        {
+            await _bannerService.TDeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult CreateBanner()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBanner(CreateBannerDto createBannerDto)
+        {
+            var newBanner = _mapper.Map<Banner>(createBannerDto);
+
+            await _bannerService.TCreateAsync(newBanner);
+            return RedirectToAction("Index");
         }
     }
 }
