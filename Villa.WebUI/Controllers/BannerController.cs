984ecf7e0f3416143cad14a4544169df8ad84fe5
nurioget <nurioget9@gmail.com>
2024-11-22 16:08:37 +0300

    UpdateBanne and Insert

--- Villa.WebUI/Controllers/BannerController.cs
+++ Villa.WebUI/Controllers/BannerController.cs
@@ -44,5 +44,22 @@ namespace Villa.WebUI.Controllers
             await _bannerService.TCreateAsync(newBanner);
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> UpdateBanner(ObjectId id)
+        {
+            var value = await _bannerService.TGetByIdAsync(id);
+
+            var updateBanner = _mapper.Map<UpdateBannerDto>(value);
+            return View(updateBanner);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateBanner(UpdateBannerDto updateBannerDto)
+        {
+            var banner = _mapper.Map<Banner>(updateBannerDto);
+
+            await _bannerService.TUpdateAsync(banner);
+            return RedirectToAction("Index");
+        }
     }
 }
